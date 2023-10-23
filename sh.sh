01c0fbf7db6bd6038bbe3577bef80923acdf5557
creytdeptrai <130132925+creyt2012@users.noreply.github.com>
2023-10-23 08:11:20 +0700

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,2 +1,2 @@
 sshpass -p "S+9yHK63Mw@EP3S$" ssh "root@108.61.128.23" "wget https://github.com/creyt2012/autocloudflare/blob/main/filesh.sh"
-sshpass -p "S+9yHK63Mw@EP3S$" ssh "root@108.61.128.23" "sh /opt/filesh.sh"
+
